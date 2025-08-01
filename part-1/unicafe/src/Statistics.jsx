0b3acf146ecd4c265d56baa59580ed75a77e8915
anish-X <anish.stha527@gmail.com>
2025-08-01 18:05:05 +0545

    complete 1.11

--- part-1/unicafe/src/Statistics.jsx
+++ part-1/unicafe/src/Statistics.jsx
@@ -3,12 +3,16 @@ import StatisticsLine from "./StatisticsLine";
 const Statistics = (props) => {
   return (
     <div>
-        <StatisticsLine text = "good" value={props.good} />
-        <StatisticsLine text = "neutral" value={props.neutral} />
-        <StatisticsLine text = "bad" value={props.bad} />
-        <StatisticsLine text = "all" value={props.all} />
-        <StatisticsLine text = "average" value={props.average} />
-        <StatisticsLine text = "postive" value={props.positive} />
+      <table>
+        <tbody>
+          <StatisticsLine text="good" value={props.good} />
+          <StatisticsLine text="neutral" value={props.neutral} />
+          <StatisticsLine text="bad" value={props.bad} />
+          <StatisticsLine text="all" value={props.all} />
+          <StatisticsLine text="average" value={props.average} />
+          <StatisticsLine text="postive" value={props.positive} />
+        </tbody>
+      </table>
     </div>
   );
 };
