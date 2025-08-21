878b7609ce90d8468ebecddf3556b48d556b655f
anish-X <anish.stha527@gmail.com>
2025-08-21 18:57:40 +0545

    feat: axios for create read update delete

---
+++ part-2/phonebook/src/services/service.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const baseURL = 'http://localhost:3001/persons';
+
+const getAll = () => {
+    const request = axios.get(baseURL);
+    return request.then(response => response.data)
+};
+
+const deleteById = (id) => {
+    const request = axios.delete(`${baseURL}/${id}`);
+    return request.then(response => response.data);
+};
+
+const updateById = (id, newObject) => {
+    const request = axios.put(`${baseURL}/${id}`, newObject);
+    return request.then(response => response.data)
+}
+
+const create = (newObject) => {
+    const request = axios.post(`${baseURL}`, newObject);
+    return request.then(response => response.data)
+}
+
+export default {
+    getAll, deleteById, updateById, create
+}
+
